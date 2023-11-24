ea3de23c7afc0fa974271fef4a12133f513fabf4
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-24 20:13:25 +0530

    Create 02-concatenate-two-strings.py

---
+++ 02-concatenate-two-strings.py
@@ -0,0 +1,9 @@
+import time
+
+print("\nProgram to concatenate 2 strings\n")
+time.sleep(2)
+
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+print(string1 + " " + string2)
