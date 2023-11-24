7c12fcebea90f7d3499dbc1dd59ff6f49102fccc
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-24 20:09:01 +0530

    Create user-input-string-to-uppercase.py

---
+++ user-input-string-to-uppercase.py
@@ -0,0 +1,7 @@
+import time
+
+print("\nProgram to convert a string to uppercase\n")
+time.sleep(2)
+
+string = input("Enter the string which you want converted into uppercase: ").upper()
+print(string)
