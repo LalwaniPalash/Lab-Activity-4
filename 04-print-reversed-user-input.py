8cc5d557bb70ae354a9955155f23cf7d360d8adc
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-24 20:37:14 +0530

    Create 04-print-reversed-user-input.py

---
+++ 04-print-reversed-user-input.py
@@ -0,0 +1,14 @@
+import time
+
+print("\nProgram to reverse user inputted string.\n")
+time.sleep(2)
+
+userinput = None
+
+while userinput is None:
+	try: 
+		userinput = str(input("Enter a string: ")).lower()
+	except ValueError:
+		print("Error: Invalid Input!")
+
+print(userinput[::-1])
